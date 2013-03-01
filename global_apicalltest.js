98e7105efde682d2c5e5b2a41964e29d6439d28e
abhisekp <abhisekp@engineer.com>
2013-03-01 17:21:33 +0530

    Corrected presentational view

--- global_apicalltest.js
+++ global_apicalltest.js
@@ -6,6 +6,7 @@
 		var apiRequest = document.getElementById("apiRequest");
 		var apiRequestSubmit = document.getElementById("apiRequestSubmit");
 		var container = document.getElementById("container");
+		var outputXML = document.getElementById("outputXML");
 		var apiURLBar_defVal = "Enter API Request URL";
 
 		apiRequest.onsubmit = function () {
@@ -14,17 +15,34 @@
 			return false;
 		};
 
-		apiURLBar.onfocus = container.ondrag = function () {
+		apiRequest.onreset = function () {
+			outputXML.style.visibility = "hidden";
+			container.style.cursor = "wait";
+			container.style.position = "absolute";
+			container.style.marginLeft = "-200px";
+			container.style.marginTop = "-50px";
+			container.style.top = "50%";
+			container.style.left = "50%";
+		};
+
+		apiURLBar.onfocus = function () {
 			apiURLBar.focus();
 			if (apiURLBar.value === apiURLBar_defVal) {
 				apiURLBar.value = "";
 			}
 		};
+
+		container.ondrag = function () {
+			apiURLBar.focus();
+			apiURLBar.value = "";
+		};
+
 		apiURLBar.onblur = function () {
 			if (apiURLBar.value === "") {
 				apiURLBar.value = apiURLBar_defVal;
 			}
 		};
+
 		apiURLBar.focus();
 	};
 }());
